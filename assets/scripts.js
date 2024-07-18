b2c423c0eddc32bf992be613606a3b6c1dadd06f
Youssef Nazeer <youssefnazeer@gmail.com>
2024-07-18 15:08:54 +0200

    Modification rich snippet + image og

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,22 +13,3 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
-
-// Définir les données structurées en JavaScript
-const data = {
-    "@context": "http://schema.org",
-    "@type": "Organization",
-    "name": "Nina Carducci - Photographe",
-    "address": {
-      "@type": "PostalAddress",
-      "streetAddress": "68 avenue Alsace-Lorraine",
-      "postalCode": "33200",
-      "addressLocality": "Bordeaux"
-    }
-  };
-  const jsonData = JSON.stringify(data);
-
-  const script = document.createElement('script');
-  script.type = 'application/ld+json';
-  script.text = jsonData;
-  document.head.appendChild(script);
